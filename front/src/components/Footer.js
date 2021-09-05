9c9a9becdd71e5245c6140ab4ae6b0d46e039e36
Madat Bay <medet.bayramov22@gmail.com>
2021-09-05 21:42:31 +0400

    footer layout changes

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,11 +1,30 @@
-import { Grid, Link } from "@material-ui/core";
+import { Link } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LanguageIcon from "@material-ui/icons/Language";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import React from "react";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <p> Made with ❤️</p>
-      <Link>by Madat Bay</Link>
+      <p>
+        Made with ❤️
+        <Link href="https://madatbay.com" target="_blank">
+          {" "}
+          by Madat Bay
+        </Link>
+      </p>
+      <div className="link-group">
+        <Link href="https://github.com/madatbay">
+          <GitHubIcon />
+        </Link>
+        <Link href="https://madatbay.com">
+          <LanguageIcon />
+        </Link>
+        <Link href="https://twitter.com/madatbay">
+          <TwitterIcon />
+        </Link>
+      </div>
     </div>
   );
 }
