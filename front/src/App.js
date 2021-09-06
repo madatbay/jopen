3004a357a6ddfbb5fc6136d4a699f80b91e6af1c
Madat Bay <medet.bayramov22@gmail.com>
2021-09-06 22:44:52 +0400

    material theme provider added

--- front/src/App.js
+++ front/src/App.js
@@ -6,9 +6,28 @@ import BoardCreate from "./components/BoardCreate";
 import BoardDetail from "./components/BoardDetail";
 import Footer from "./components/Footer";
 import NoMatch from "./components/NoMatch";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#039be5',
+      dark: '#006db3',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Navbar />
       <Container>
@@ -27,6 +46,7 @@ function App() {
       </Container>
       <Footer />
     </Router>
+    </ThemeProvider>
   );
 }
 
